cabf530279f5fcc43c3feda0997459c34951dc52
tikopb <tiko.pb@gmail.com>
2022-06-05 09:35:57 +0700

    first dev

---
+++ variabel.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string //-> penulisan variabel tanpa pengisian value
+
+	name = "tiko putra"
+	fmt.Println(name)
+
+	name = "TIKO PUTRA B"
+	fmt.Println(name)
+
+	var number int
+	number = 30
+	fmt.Println(number)
+
+	country := "indonesia" //-> declare tanpa penulisan var
+	fmt.Println(country)
+}
